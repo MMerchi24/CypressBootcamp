0d64727d45871b8e792a5caa9be3e6be7e9948dc
MMerchi24 <merxi_83@hotmail.com>
2024-06-03 20:01:37 +0200

    gitchangeartefact

--- cypress/e2e/step-definitions/loginSteps.js
+++ cypress/e2e/step-definitions/loginSteps.js
@@ -1,3 +1,4 @@
+import 'cypress-mochawesome-reporter/cucumberSupport';
 import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
 import { LoginPage } from "../pages/loginPage"
 import { CommonPage } from "../pages/commonPage"
